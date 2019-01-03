ebab0fda35412137cce160e70f057d19f28cd58e
yinm <yinm.cminor9@gmail.com>
2019-01-03 10:40:41 +0900

    Add Home page

---
+++ src/views/pages/Home.js
@@ -0,0 +1,37 @@
+const getPostsList = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const response = await fetch('https://5bb634f6695f8d001496c082.mockapi.io/api/posts', options)
+    return await response.json
+  } catch (err) {
+    console.error('Error getting documents', err)
+  }
+}
+
+const Home = {
+  render: async () => {
+    const posts = await getPostsList()
+
+    return /*html*/`
+      <section class="section">
+        <h1>Home</h1>
+        <ul>
+          ${ posts.map(post =>
+               `<li><a href="#/p/${post.id}">${post.title}</a></li>`
+             ).join('\n ')
+          }
+        </ul>
+      </section>
+    `
+  },
+
+  after_render: async () => {}
+}
+
+export default Home
